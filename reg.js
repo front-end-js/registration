e8364dee97be58a044da99e34bf52b975e706638
Teacher <softmasterlab@hotmail.com>
2020-07-08 21:26:18 +0300

    Изменены reg.css, reg.html. reg.js, server/handler.html

--- reg.js
+++ reg.js
@@ -1,1 +1,15 @@
 'use strict';
+
+window.addEventListener('load', () => {
+
+    let form = document.getElementById('form1');
+    form.addEventListener('submit', () => {
+        let access = confirm('Вы уверены в надежности данных, введенных в форму?');
+        if (access === true) {
+            form.submit();
+        } else {
+            alert('Данные не надежны! Отправка заблокирована!');
+        }
+    });
+
+});
